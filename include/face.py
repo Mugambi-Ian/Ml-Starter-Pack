f37040dd5f360e7fb7d11051339f2960b195ab80
mugambi-ian <linksian63@gmail.com>
2020-05-20 16:09:55 +0300

    ml-update

--- include/face.py
+++ include/face.py
@@ -1,23 +1,29 @@
 import cv2
 
-eye_cascade = cv2.CascadeClassifier("include/xml/haarcascade_eye.xml")
-face_cascade = cv2.CascadeClassifier(
-    "include/xml/haarcascade_frontalface_default.xml")
-src = cv2.imread("include/img/test.jpg");l
-img = cv2.resize(src, (int(src.shape[1]/2), int(src.shape[0]/2)))
+faceCascade = cv2.CascadeClassifier("include/xml/face.xml")
+eyeCascade = cv2.CascadeClassifier("include/xml/eye.xml")
+noseCascade = cv2.CascadeClassifier("include/xml/nose.xml")
+
+src = cv2.imread("include/img/test.jpg")
+img = cv2.resize(src, (int(src.shape[1]/3), int(src.shape[0]/3)))
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.1)
+faces = faceCascade.detectMultiScale(gray, 1.1)
+coords = []
 for (x, y, w, h,) in faces:
     roi_gray = gray[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray, 1.25)
-    if len(eyes) > 1 & len(eyes) < 2:
-        print(eyes)
+    eyes = eyeCascade.detectMultiScale(roi_gray, 1.25)
+    nose = noseCascade.detectMultiScale(roi_gray, 1.25)
+    if len(eyes) == 2 and len(nose) == 1:
+        coords = [x, y, w, h]
         face_ = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_color = face_[y:y+h, x:x+w]
         for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+            cv2.rectangle(roi_color, (ex, ey),
+                          (ex+ew, ey+eh), (0, 255, 0), 2)
+        for (ex, ey, ew, eh) in nose:
+            cv2.rectangle(roi_color, (ex, ey),
+                          (ex+ew, ey+eh), (255, 255, 0), 2)
 
-
-cv2.imshow('img', img)
+cv2.imshow('Test', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- include/video.py
+++ include/video.py
@@ -2,24 +2,30 @@ import cv2
 import time
 
 
+faceCascade = cv2.CascadeClassifier("include/xml/face.xml")
+eyeCascade = cv2.CascadeClassifier("include/xml/eye.xml")
+noseCascade = cv2.CascadeClassifier("include/xml/nose.xml")
+
 video = cv2.VideoCapture(0)
 while True:
     check, img = video.read()
-    eye_cascade = cv2.CascadeClassifier("include/xml/haarcascade_eye.xml")
-    face_cascade = cv2.CascadeClassifier(
-        "include/xml/haarcascade_frontalface_default.xml")
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.1)
+    faces = faceCascade.detectMultiScale(gray, 1.1)
+    coords = []
     for (x, y, w, h,) in faces:
         roi_gray = gray[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.25)
-        if len(eyes) > 1 & len(eyes) < 2:
-            print(eyes)
+        eyes = eyeCascade.detectMultiScale(roi_gray, 1.22)
+        nose = noseCascade.detectMultiScale(roi_gray, 1.22)
+        if len(eyes) == 2 and len(nose) == 1:
+            coords = [x, y, w, h]
             face_ = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
             roi_color = face_[y:y+h, x:x+w]
             for (ex, ey, ew, eh) in eyes:
                 cv2.rectangle(roi_color, (ex, ey),
-                              (ex+ew, ey+eh), (0, 255, 0), 2)
+                            (ex+ew, ey+eh), (0, 255, 0), 2)
+            for (ex, ey, ew, eh) in nose:
+                cv2.rectangle(roi_color, (ex, ey),
+                            (ex+ew, ey+eh), (255, 255, 0), 2)
     cv2.imshow("camera-stream", img)
     key = cv2.waitKey(1)
     if key == ord('q'):
